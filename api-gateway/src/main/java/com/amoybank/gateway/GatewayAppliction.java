a2dfb3e07d3a23353cab0ea6c298b37b2137f4ce
SumJcj <taxibus@sina.com>
2018-10-30 17:37:05 +0800

    1. 修改工程名 metric-api -> pay_metric-api 2. 修改启动类名：  MetricApiProvider -> PayMetricApiApplication

--- api-gateway/src/main/java/com/amoybank/gateway/GatewayAppliction.java
+++ api-gateway/src/main/java/com/amoybank/gateway/GatewayAppliction.java
@@ -1,13 +1,20 @@
 package com.amoybank.gateway;
 
+import com.amoybank.gateway.filter.AccessFilter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class GatewayAppliction {
     public static void main(String[] args) {
         new SpringApplicationBuilder(GatewayAppliction.class).web(true).run(args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 }
--- pay-metric-api/src/main/java/com/amoybank/metric/PayMetricApiApplication.java
+++ pay-metric-api/src/main/java/com/amoybank/metric/PayMetricApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class MetircApiProvider {
+public class PayMetricApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MetircApiProvider.class, args);
+        SpringApplication.run(PayMetricApiApplication.class, args);
     }
 }
